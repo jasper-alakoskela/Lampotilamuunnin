8695d56364a3a45be6027e0a6ab838c62621fda4
Jasper Alakoskela <jasper.alakoskela@itvelhot.local>
2020-09-02 13:54:46 +0300

    kohta valmista

--- muunnin.js
+++ muunnin.js
@@ -1,11 +1,14 @@
-const chooseTemperature = document.getElementById('temperature').value;
-let input = document.getElementById("input");
 const calculateBtn = document.getElementById('calculate');
-const chooseDecimal = document.getElementsByName('decimal').value;
-let vastaus= '';
 
 calculateBtn.addEventListener("click", function(){
         
+    const chooseTemperature = document.getElementById('temperature').value;
+    const chooseDecimal = document.querySelector('input[name="decimal"]:checked').value;
+    let input = document.getElementById("input").value;
+    
+
+    vastaus = muunna(input, chooseTemperature);
+
     if (chooseDecimal == '1d') {
         console.log(vastaus.toFixed(1));
     }
@@ -17,22 +20,40 @@ calculateBtn.addEventListener("click", function(){
     }
     else {
         console.log(vastaus);
-    }      
+    }  
+    
+
 });
 
-function muunna() {
-     x = input.value;
+function muunna(x, chooseTemperature) {
 
     if (chooseTemperature == 'ctof') {
         console.log(x * 1.8 + 32);
-        vastaus = x * 1.8 + 32;
+        return x * 1.8 + 32;
     }
     else if (chooseTemperature == 'ftoc') {
         console.log((x - 32) / 1.8);
-        vastaus = (x - 32) / 1.8;
+        return (x - 32) / 1.8;
+    }
+    else if (chooseTemperature == 'ftok') {
+        console.log((x + 459,67) / 1.8);
+        return (x + 459,67) / 1.8;
+    }
+    else if (chooseTemperature == 'ktof') {
+        console.log(x * 1.8 - 459.67);
+        return x * 1.8 - 459.67;
+    }
+    else if (chooseTemperature == 'ktoc') {
+        console.log(x - 273.15);
+        return x - 273.15;
+    }
+    else if (chooseTemperature == 'ctok') {
+        console.log(x + 273,15);
+        return x + 273,15;
     }
     else {
         console.log('Valitse!');
+        return "error"
     }
 }
 
